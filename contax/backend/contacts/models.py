163f0e0620596d867381cd16e73e26732ee166af
Henry Marment <henry@adsquare.com>
2019-11-12 21:52:59 +0100

    add first models

--- contax/backend/contacts/models.py
+++ contax/backend/contacts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    date_of_birth = models.DateField()
+    created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+
+class EmailAddress(models.Model):
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    email_address = models.EmailField()
+    created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+
+class PhoneNumber(models.Model):
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+class PostalAddress(models.Model):
+    name = models.CharField(max_length=200)
+    street = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    post_code = models.CharField(max_length=200)
+    # post_code = models.RegexField(regex=r'[0-9]{4,5}|[A-Z]{1,2}[0-9R][0-9A-Z]? [0-9][A-Z]{2}')
+    country = models.CharField(max_length=2)
+    contacts = models.ManyToManyField(Contact)
+    created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
