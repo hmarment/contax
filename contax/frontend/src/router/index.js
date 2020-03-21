993bba2bd1edc82ce8b22362dc5f3ef73c45b1bb
Henry Marment <henry@adsquare.com>
2020-03-21 19:04:26 +0100

    Redesign contacts list to use bootstrap cards

--- contax/frontend/src/router/index.js
+++ contax/frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/components/Index'
+import Contacts from '@/components/Contacts'
 import ContactEdit from '@/components/ContactEdit'
 
 Vue.use(Router)
@@ -10,12 +10,12 @@ export default new Router({
     {
       path: '/',
       name: 'contacts',
-      component: Index
+      component: Contacts
     },
     {
       path: '/contacts',
       name: 'contacts',
-      component: Index
+      component: Contacts
     },
     {
       path: '/contacts/edit/:id',
